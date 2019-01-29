6a5d31e1524e89567b52e126a06e16737a849a52
Te-Wei Tsai <TTsai@lsst.org>
2019-01-29 13:07:45 -0700

    Add the setup.py.

---
+++ python/lsst/ts/aoclcSim/WepPhosimCmpt.py
@@ -0,0 +1,8 @@
+class WepPhosimCmpt(object):
+
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="AoclcSimulator",
+    version="0.1.0",
+    author="Te-Wei Tsai",
+    author_email="ttsai@lsst.org",
+    packages=["lsst.ts.aoclcSim"]
+)
